e37948dcca95081b70cfe5486ab3ec2a6a72556b
Fatma-by <fatmabenyouness@gmail.com>
2023-12-14 19:28:23 +0100

    creerClassDone

--- src/Components/CreerClass/Sidebar.jsx
+++ src/Components/CreerClass/Sidebar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
+import Modals from "../Modals/Modals";
 
 function Sidebar() {
+  const [show,setShow] = useState(false)
   const handleToggleSidebar = () => {
     const sidebar = document.querySelector("#sidebar");
     sidebar.classList.toggle("collapsed");
@@ -18,7 +20,13 @@ function Sidebar() {
           <ul className="sidebar-nav">
             <li className="sidebar-header">Mes Classes </li>
             <li className="sidebar-item">
-              <a href="#" className="sidebar-link">
+              <a
+                href="#"
+                className="sidebar-link"
+                onClick={() => {
+                 setShow(true)
+                }}
+              >
                 <i className=" pe-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -67,7 +75,6 @@ function Sidebar() {
                   >
                     <path d="M337.8 5.4C327-1.8 313-1.8 302.2 5.4L166.3 96H48C21.5 96 0 117.5 0 144V464c0 26.5 21.5 48 48 48H256V416c0-35.3 28.7-64 64-64s64 28.7 64 64v96H592c26.5 0 48-21.5 48-48V144c0-26.5-21.5-48-48-48H473.7L337.8 5.4zM96 192h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H96c-8.8 0-16-7.2-16-16V208c0-8.8 7.2-16 16-16zm400 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H512c-8.8 0-16-7.2-16-16V208zM96 320h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H96c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16zm400 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H512c-8.8 0-16-7.2-16-16V336zM232 176a88 88 0 1 1 176 0 88 88 0 1 1 -176 0zm88-48c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H336V144c0-8.8-7.2-16-16-16z" />
                   </svg>
-                  
                 </i>
                 La classe de M/Mme{" "}
               </a>
@@ -93,6 +100,7 @@ function Sidebar() {
             <div className="mb-3"></div>
           </div>
         </main>
+        {show && <Modals show={show  } setShow={setShow}/>}
       </div>
     </div>
   );
---
+++ src/Components/Modals/Modals.jsx
@@ -0,0 +1,237 @@
+import { useRef, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+function Modals({ show, setShow }) {
+    const [selectedMatiere,SetslectedMatiere ]= useState()
+    const [selectedNiveau,SetslectedNiveau ]= useState()
+    const [className, setClassName]= useState()
+    const ref = useRef()
+
+    const handleDropdownModifyNiveau = (event) => {
+      SetslectedNiveau(event.target.textContent);
+    };
+    const handleDropdownModify = (event) => {
+      SetslectedMatiere(event.target.textContent);
+    };
+    const handleSave = async () => {
+      console.log({ className, selectedMatiere, selectedNiveau});
+      axios
+        .post(`/api/classes/nouvelle-classe`, {
+          NomClass: className,
+          Matiere : selectedMatiere,
+           Niveau :selectedNiveau
+         
+        })
+        .then(async (response) => {
+          if (response.data) toast.success("Classe crée avec succés");
+        })
+        .catch(function (error) {
+          //axios handel error
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            toast.error("Classe déjà existant");
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+        });
+    };
+
+    
+
+
+
+  return (
+    <>
+      <Modal show={show} onHide={show}>
+        <Modal.Header closeButton>
+          <Modal.Title>Nouvelle Classe</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Nom de la Classe</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="nom de la classe"
+                ref={ref}
+
+                onChange={(e) => {
+               setClassName(e.currentTarget.value);
+                }}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Niveau Educatif</Form.Label>
+              <div className="dropdown">
+                    <button
+                      style={{
+                        width: "100%",
+                        backgroundColor: "white",
+                        border: "#dee2e6 1px solid",
+                      }}
+                      type="button"
+                      className="btn btn-info dropdown-toggle"
+                      data-bs-toggle="dropdown"
+                     
+
+                    >
+                      {" "}
+                      {selectedNiveau== null ? "Niveau Educatif" : selectedNiveau}
+                      
+                    </button>
+                    <ul style={{ width: "100%" }} className="dropdown-menu">
+                      <li>
+                        {" "}
+                        <a
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleDropdownModifyNiveau}
+                          
+                        >
+                            Première Année
+                        </a>{" "}
+                      </li>
+                      <li>
+                        {" "}
+                        <a
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleDropdownModifyNiveau}
+
+                         
+                        >
+                          {" "}
+                          Deuxième Année{" "}
+                        </a>{" "}
+                        <a
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleDropdownModifyNiveau}
+
+                         
+                        >
+                          {" "}
+                          Troisième Année{" "}
+                        </a>{" "}
+                        {" "}
+                        <a
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleDropdownModifyNiveau}
+
+                         
+                        >
+                          {" "}
+                          Quatrième Année{" "}
+                        </a>{" "}
+                        {" "}
+                        <a
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleDropdownModifyNiveau}
+
+                         
+                        >
+                          {" "}
+                          Cinquième Année{" "}
+                        </a>{" "}
+                        {" "}
+                        <a
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleDropdownModifyNiveau}
+
+                         
+                        >
+                          {" "}
+                          Sixième Année{" "}
+                        </a>{" "}
+                      </li>
+                    </ul>
+                  </div>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Matières</Form.Label>
+              <div className="dropdown">
+                    <button
+                      style={{
+                        width: "100%",
+                        backgroundColor: "white",
+                        border: "#dee2e6 1px solid",
+                      }}
+                      type="button"
+                      className="btn btn-info dropdown-toggle"
+                      data-bs-toggle="dropdown"
+                     
+
+                    >
+                      {" "}
+                      {selectedMatiere== null ? "Matiere" : selectedMatiere}
+                      
+                    </button>
+                    <ul style={{ width: "100%" }} className="dropdown-menu">
+                      <li>
+                        {" "}
+                        <a
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleDropdownModify}
+                          
+                        >
+                            Arabe
+                        </a>{" "}
+                      </li>
+                      <li>
+                        {" "}
+                        <a
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleDropdownModify}
+
+                         
+                        >
+                          {" "}
+                          Science{" "}
+                        </a>{" "}
+                        <a
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleDropdownModify}
+
+                         
+                        >
+                          {" "}
+                          Français{" "}
+                        </a>{" "}
+                      </li>
+                    </ul>
+                  </div>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShow(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default Modals;
